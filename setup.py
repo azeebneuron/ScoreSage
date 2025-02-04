0a94bc6d7a678d1f60b1ea700313d13e6744d327
rahul <srahulsingh7488@gmail.com>
2025-02-05 04:40:09 +0530

    Make package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="scoresage",
+    version="0.0.1",
+    author="azeebneuron",
+    author_email="srahulsingh7488@email.com",
+    packages=find_packages(),
+    install_requires=[]
+)
